90a980527bc3ff8a0b1467be395107f0952376d3
rokyby <127371152+rokyby@users.noreply.github.com>
2025-05-15 15:00:40 -0500

    Update Convert Csv To Json.py

--- Convert Csv To Json.py
+++ Convert Csv To Json.py
@@ -3,9 +3,9 @@ import opendatasets as od
 dataset_link="https://www.kaggle.com/datasets/joebeachcapital/top-1000-steam-games"
 od.download(dataset_link)
 
-import os
+"""import os
 os.chdir("top-1000-steam-games")
-os.listdir()
+os.listdir()"""
 
 import pandas as pd
 archivo="steam_app_data.csv"
@@ -18,7 +18,7 @@ archivo = "steam_app_data.csv"
 df = pd.read_csv(archivo)
 
 # Seleccionar las columnas relevantes
-columnas = ["name"],["detailed_description"],["short_description"],["categories"],["genres"]
+columnas = ("name","detailed_description","short_description","categories","genres")
 df = df[columnas]
 
 # Convertir a JSON y guardar
